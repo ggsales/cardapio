b4de1983ba2e3537571980ae7a9020fa73c31809
ggsales <giovanisales14@gmail.com>
2022-05-10 14:42:33 -0300

    apply normalize css and create header

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Cardapio from './pages/Cardapio';
-
+import "normalize.css"
 
 
 const root = ReactDOM.createRoot(
--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,14 +1,20 @@
 
 import styles from "./Cardapio.module.scss";
-import {ReactComponent as Logo} from "assets/logo.svg"
+import { ReactComponent as Logo } from "assets/logo.svg"
 
 
-export default function Cardapio(){
-    return(
+export default function Cardapio() {
+    return (
         <main>
             <nav className={styles.menu}>
                 <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código da massa
+                </div>
+
+            </header>
         </main>
     )
 }
