e7df77003366906fb034502a1912b820ab8cb594
ggsales <giovanisales14@gmail.com>
2022-05-15 01:26:32 -0300

    create and apply classNames package

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -1,9 +1,15 @@
 
 import styles from "./Cardapio.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg"
+import Buscador from "./Buscador";
+import { useState } from "react";
+import Filtros from "./Filtros";
 
 
 export default function Cardapio() {
+    const [busca, setBusca] =   useState("");
+    const [filtro, setFiltro] = useState<number | null>(null)
+
     return (
         <main>
             <nav className={styles.menu}>
@@ -13,8 +19,14 @@ export default function Cardapio() {
                 <div className={styles.header__text}>
                     A casa do código da massa
                 </div>
-
             </header>
+            <section className={styles.cardapio}>
+                <h3 className={styles.cardapio__titulo}>Cardápio</h3>
+                <Buscador busca={busca} setBusca={setBusca}/>
+                <div className={styles.cardapio__filtros}>
+                    <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                </div>
+            </section>
         </main>
     )
 }
