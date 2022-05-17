0a4f4dc4f8d33dc146c05d1506fcde12d0419383
ggsales <giovanisales14@gmail.com>
2022-05-16 23:39:05 -0300

    create order

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -4,11 +4,13 @@ import { ReactComponent as Logo } from "assets/logo.svg"
 import Buscador from "./Buscador";
 import { useState } from "react";
 import Filtros from "./Filtros";
+import Ordenador from "./Ordenador";
 
 
 export default function Cardapio() {
     const [busca, setBusca] =   useState("");
-    const [filtro, setFiltro] = useState<number | null>(null)
+    const [filtro, setFiltro] = useState<number | null>(null);
+    const [ordenador, setOrdenador] = useState("")
 
     return (
         <main>
@@ -25,6 +27,7 @@ export default function Cardapio() {
                 <Buscador busca={busca} setBusca={setBusca}/>
                 <div className={styles.cardapio__filtros}>
                     <Filtros filtro={filtro} setFiltro={setFiltro}/>
+                    <Ordenador ordenador={ordenador} setOrdenador={setOrdenador}/>
                 </div>
             </section>
         </main>
